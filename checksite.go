7757c8e74d5e62a4f4eb635e47b7d1ca80b86765
Noy <noyhillel@gmail.com>
2017-11-14 16:34:45 +0000

    Added files.

---
+++ checksite.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+const WORKERS = 25
+
+func main() {
+	site, _ := ioutil.ReadFile("sites.txt")
+	siteFromByte := string(site)
+	urls := strings.Split(siteFromByte, "\n")
+	wg := new(sync.WaitGroup)
+	in := make(chan string, 2*WORKERS)
+
+	for i := 0; i < WORKERS; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for url := range in {
+				checkIfOnline(url)
+			}
+		}()
+	}
+	for _, url := range urls {
+		if url != "" {
+			in <- url
+		}
+	}
+	close(in)
+	wg.Wait()
+}
+
+func checkIfOnline(url string) {
+	now := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error getting site: %v", err)
+	}
+	defer resp.Body.Close()
+	fmt.Println(time.Since(now), url)
+}
